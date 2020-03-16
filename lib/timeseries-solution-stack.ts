10904522ed7f9dee8bceb44acd6c6a576ce08a33
Darren Holland <darrenh@gridconnect.com>
2020-03-16 16:46:49 -0500

    Initial commit

--- lib/timeseries-solution-stack.ts
+++ lib/timeseries-solution-stack.ts
@@ -1,9 +1,33 @@
 import * as cdk from '@aws-cdk/core';
+import * as lambda from '@aws-cdk/aws-lambda';
+import * as sfn from '@aws-cdk/aws-stepfunctions';
+import * as tasks from '@aws-cdk/aws-stepfunctions-tasks';
 
 export class TimeseriesSolutionStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const helloFunction = new lambda.Function(this, 'HelloFunction', {
+      runtime: lambda.Runtime.NODEJS_12_X,
+      handler: 'hello',
+      code: lambda.Code.asset('./handlers/hello'),
+      timeout: cdk.Duration.seconds(6)
+    });
+
+    const startTask = new sfn.Task(this, 'StartStep', {
+      task: new tasks.InvokeFunction(helloFunction)
+    });
+
+    const loop = new sfn.Pass(this,'loop!');
+    const done = new sfn.Pass(this,'done!');
+
+    const sfndef = startTask
+      .next(loop)
+      .next(done);
+    const sm = new sfn.StateMachine(this, 'HelloWorldStateMachine', {
+      definition: sfndef,
+      timeout: cdk.Duration.seconds(15)
+    });
   }
 }
+
